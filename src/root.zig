bd9b38f7cd09cf789349b90fb1f7426aca68031e
Andre Fuentes <aefuente@pm.me>
2026-01-25 08:39:57 -0600

    binding to socket

--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,26 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
+const Address = std.net.Address;
+const posix = std.posix;
 
-pub fn bufferedPrint() !void {
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-    const stdout = &stdout_writer.interface;
+pub const PORT: u16 = 8000;
+pub const ADDRESS = "0.0.0.0";
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+pub const Server = struct {
+    address: Address,
+    socket: posix.socket_t,
 
-    try stdout.flush(); // Don't forget to flush!
-}
+    pub fn init() !Server {
+        const address = try Address.parseIp4(ADDRESS, PORT);
+        const sock = try posix.socket( posix.AF.INET, posix.SOCK.STREAM | posix.SOCK.NONBLOCK, posix.IPPROTO.TCP);
+        errdefer posix.close(sock);
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+        try std.posix.bind(sock, &address.any, address.getOsSockLen());
+        return .{.address = address, .socket = sock};
+    }
 
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+    pub fn deinit(self: *Server) void {
+        posix.close(self.socket);
+    }
+};
+
+
