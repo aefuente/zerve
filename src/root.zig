80205f42eb2d6b22ecbb345789452f9d7b401807
Andre Fuentes <aefuente@pm.me>
2026-01-26 16:20:28 -0600

    200 response

--- src/root.zig
+++ src/root.zig
@@ -93,7 +93,33 @@ pub const HTTPRequest = struct {
     }
 };
 
-pub const HTTPResponse = struct { Version: []u8, StatusCode: u8, Reason: []u8, Headers: std.AutoHashMap([]u8, []u8), body: ?[]u8 };
+pub const HTTPResponse = struct {
+    Version: []const u8,
+    StatusCode: u8,
+    Reason: []const u8,
+    Headers: std.StringHashMap([]const u8),
+    body: ?[]u8,
+
+    pub fn serialize(self: HTTPResponse, allocator: Allocator) ![]u8 {
+        var writer = try std.Io.Writer.Allocating.initCapacity(allocator, 100);
+        try writer.writer.print("{s} {d} {s}\r\n", .{ self.Version, self.StatusCode, self.Reason });
+
+        if (self.Headers.count() == 0) {
+            try writer.writer.print("\r\n", .{});
+        } else {
+            var it = self.Headers.iterator();
+            while (it.next()) |value| {
+                try writer.writer.print("{s}: {s}\r\n", .{ value.key_ptr.*, value.value_ptr.* });
+            }
+            try writer.writer.print("\r\n", .{});
+        }
+
+        if (self.body) |b| {
+            try writer.writer.print("{s}", .{b});
+        }
+        return writer.toOwnedSlice();
+    }
+};
 
 pub const Server = struct {
     address: Address,
@@ -146,12 +172,18 @@ pub const Server = struct {
                     };
 
                     if (n == 0) {
-                        _ = posix.close(e.data.fd);
+                        posix.close(e.data.fd);
                     } else {
                         var request = try HTTPRequest.init(allocator, buf[0..n]);
                         request.print();
                         defer request.Headers.deinit();
-                        _ = try posix.write(e.data.fd, buf[0..n]);
+                        var response = HTTPResponse{ .Version = "HTTP/1.0", .StatusCode = 200, .Reason = "OK", .Headers = std.StringHashMap([]const u8).init(allocator), .body = null };
+                        defer response.Headers.deinit();
+                        const response_data = try response.serialize(allocator);
+                        defer allocator.free(response_data);
+
+                        _ = try posix.write(e.data.fd, response_data);
+                        posix.close(e.data.fd);
                     }
                 }
             }
