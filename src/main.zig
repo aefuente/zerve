3886ea240de98905f8cfb77d23ebdcfbc01b928d
Andre Fuentes <aefuente@pm.me>
2026-01-26 09:44:24 -0600

    parsing request

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,9 @@ const zerve = @import("zerve");
 const assert = std.debug.assert;
 
 pub fn main() !void {
-    //var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-    //defer assert(debug_allocator.deinit() == .ok);
-    //const gpa = debug_allocator.allocator();
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer assert(debug_allocator.deinit() == .ok);
+    const gpa = debug_allocator.allocator();
 
     var stdout_buf: [1024]u8 = undefined;
     var stdout = std.fs.File.stdout().writer(&stdout_buf);
@@ -16,5 +16,5 @@ pub fn main() !void {
     try stdout.interface.print("Server Listening on: {s}:{d}\n", .{ zerve.ADDRESS, zerve.PORT });
     try stdout.interface.flush();
 
-    try server.listen();
+    try server.listen(gpa);
 }
