7849b17341e07e3cf1e113b1d746d61ae6e13fec
Andre Fuentes <aefuente@pm.me>
2026-01-27 15:12:03 -0600

    optional headers and BadRequest Response

--- src/root.zig
+++ src/root.zig
@@ -93,31 +93,48 @@ pub const HTTPRequest = struct {
     }
 };
 
+const BadRequest = HTTPResponse{
+    .Version = "HTTP/1.0",
+    .StatusCode = 400,
+    .Reason = "Bad Request",
+    .Headers = null,
+    .body = null,
+};
+
+
 pub const HTTPResponse = struct {
     Version: []const u8,
-    StatusCode: u8,
+    StatusCode: u16,
     Reason: []const u8,
-    Headers: std.StringHashMap([]const u8),
+    Headers: ?std.StringHashMap([]const u8),
     body: ?[]u8,
 
     pub fn serialize(self: HTTPResponse, allocator: Allocator) ![]u8 {
         var writer = try std.Io.Writer.Allocating.initCapacity(allocator, 100);
         try writer.writer.print("{s} {d} {s}\r\n", .{ self.Version, self.StatusCode, self.Reason });
-
-        if (self.Headers.count() == 0) {
-            try writer.writer.print("\r\n", .{});
-        } else {
-            var it = self.Headers.iterator();
-            while (it.next()) |value| {
-                try writer.writer.print("{s}: {s}\r\n", .{ value.key_ptr.*, value.value_ptr.* });
+        if (self.Headers) |headers | {
+            if (headers.count() == 0) {
+                try writer.writer.print("\r\n", .{});
+            } else {
+                var it = headers.iterator();
+                while (it.next()) |value| {
+                    try writer.writer.print("{s}: {s}\r\n", .{ value.key_ptr.*, value.value_ptr.* });
+                }
+                try writer.writer.print("\r\n", .{});
             }
-            try writer.writer.print("\r\n", .{});
+
+            if (self.body) |b| {
+                try writer.writer.print("{s}", .{b});
+            }
         }
 
-        if (self.body) |b| {
-            try writer.writer.print("{s}", .{b});
-        }
         return writer.toOwnedSlice();
+    }
+
+    pub fn deinit(self: *HTTPResponse) void {
+        if (self.Headers) |_| {
+            self.Headers.?.deinit();
+        }
     }
 };
 
@@ -178,7 +195,8 @@ pub const Server = struct {
                         request.print();
                         defer request.Headers.deinit();
                         var response = HTTPResponse{ .Version = "HTTP/1.0", .StatusCode = 200, .Reason = "OK", .Headers = std.StringHashMap([]const u8).init(allocator), .body = null };
-                        defer response.Headers.deinit();
+
+                        defer response.deinit();
                         const response_data = try response.serialize(allocator);
                         defer allocator.free(response_data);
 
